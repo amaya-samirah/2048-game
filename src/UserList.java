22665dcf9047c7c28077a67480ed1d19a9b701f1
amaya-samirah <amaya.s.26292@gmail.com>
2025-03-03 15:28:32 -0500

    Updated UserList class

---
+++ src/UserList.java
@@ -0,0 +1,48 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * This class deals with all the users
+ * @author Amaya Shabazz
+ */
+public class UserList {
+
+    private static UserList instance;
+    private ArrayList<User> users;
+
+    private UserList() {
+
+    }
+
+    public static synchronized UserList getInstance() {
+        if (instance == null) {
+            instance = new UserList();
+        }
+
+        return instance;
+    }
+
+    /**
+     * Creates account for new user
+     * @param userName User's chosen username
+     * @param password User's chosen password
+     * @return If account creation was successful
+     */
+    public boolean createAccount(String userName, String password) {
+        if (userName == null || password == null) {
+            return false;
+        }
+
+        for (User user : users) {
+            if (user.getUserName().equals(userName)) {
+                return false;
+            }
+        }
+
+        User newUser = new User(UUID.randomUUID(), userName, password);
+        return true;
+    }
+    
+}
