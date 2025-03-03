8eb66e4d9a4166f735e4d950f5fef670db161075
amaya-samirah <amaya.s.26292@gmail.com>
2025-03-03 15:19:45 -0500

    Updated User class

---
+++ src/User.java
@@ -0,0 +1,38 @@
+package src;
+
+import java.util.UUID;
+
+/**
+ * This class deals with the individual data of the user
+ * @author Amaya Shabazz
+ */
+public class User {
+    
+    private final UUID id;
+    private String userName;
+    private String password;
+
+    /**
+     * Contructor for a new user
+     * @param id User's id
+     * @param userName User's chosen username
+     * @param password User's chose password
+     */
+    public User(UUID id, String userName, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
