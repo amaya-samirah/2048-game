110bf696bac83c3044b418ad370797ffa4ddbbb0
amaya-samirah <amaya.s.26292@gmail.com>
2025-03-03 13:56:49 -0500

    Added functions to GameMode

--- GameMode.java
+++ GameMode.java
@@ -1,3 +1,14 @@
-public class GameMode {
-    
+/**
+ * This interface deals with what mode the game is in
+ * @author Amaya Shabazz
+ */
+
+// MODES
+// 128
+// 256
+// 512
+// 1024
+// 2048
+public interface GameMode {
+    public void hasWon();
 }
