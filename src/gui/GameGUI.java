04a4e1ba4897035e6c93cdf3e95a407c7cad03b2
amaya-samirah <amaya.s.26292@gmail.com>
2025-03-04 01:33:01 -0500

    Added home gui

--- src/gui/GameGUI.java
+++ src/gui/GameGUI.java
@@ -1,5 +1,7 @@
 package gui;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,14 +13,26 @@ import javafx.stage.Stage;
  * @author Amaya Shabazz
  */
 public class GameGUI extends Application {
-    
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/scenes/home.fxml"));
 
-        Scene scene = new Scene(root);
+    private static Scene scene;
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        //Parent root = FXMLLoader.load(getClass().getResource("/scenes/home.fxml"));
+
+        scene = new Scene(loadFXML("home"), 640, 480);
 
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GameGUI.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
