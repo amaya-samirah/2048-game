b43365c83979c85b7492d19ac5ed901c0b29f608
amaya-samirah <amaya.s.26292@gmail.com>
2025-03-04 00:30:02 -0500

    Updates to GameManager

--- src/code/GameManager.java
+++ src/code/GameManager.java
@@ -11,7 +11,70 @@ public class GameManager {
 
     public GameManager() {
         board = GameBoard.getInstance();
-        board.setMode(getMode());
+    }
+
+    public void run() {
+        println("---WELCOME TO 2048!!!---");
+        boolean quit = false;
+
+        while (!quit) {
+            quit = play();
+        }
+
+        println("---GOODBYE!!!---");
+    }
+
+    public boolean play() {
+
+        int mode = getMode();
+        board.setMode(mode);
+
+        drawBoard();
+
+        boolean play = true;
+        while (play) {
+            println("----------------");
+            println("Current Score: "+board.getScore());
+            println("----------------");
+            println("""
+                    Enter 1 to move up:
+                    Enter -1 to move down
+                    Enter 2 to move right:
+                    Enter -2 to move left:
+                    Enter 0 to quit:""");
+            println("----------------");
+            int input = keyboard.nextInt();
+            keyboard.nextLine();
+            if (input == 0) {
+                play = false;
+                break;
+            }
+            drawBoard();
+            
+            if (board.moveBlocks(input)) {
+                println("---YOU WIN!!!---");
+                play = false;
+            }
+            if (board.hasLost()) {
+                println("---YOU LOSE!!!---");
+                play = false;
+            }
+        }
+        
+        println("---GAME OVER---");
+        return play;
+    }
+
+        
+    
+
+    public void drawBoard() {
+        for (int y = 0; y < 4; y++) {
+            for (int x = 0; x < 4; x++) {
+                print("["+board.getBlock(y, x)+"]");
+            }
+            println("");
+        }
     }
 
     private int getMode() {
